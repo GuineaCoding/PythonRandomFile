a586004d552cae754b458d3edafe5a7360cf6b65
Andrian Barbulat <andrian.barbulat@gmail.com>
2024-11-25 20:58:52 +0000

    updated functionality

--- randomFile.py
+++ randomFile.py
@@ -1,7 +1,17 @@
 import secrets
 import numpy as np
 
-random_bytes = secrets.token_bytes(2048)
+random_byte_size = 2048
+
+random_bytes = secrets.token_bytes(random_byte_size)
+
 array = np.frombuffer(random_bytes, dtype=np.uint8)
-print("Array mean:", np.mean(array))
-array.tofile("random_data_python.bin")
+
+print("Array Statistics:")
+print(f"- Mean: {np.mean(array):.2f}")
+print(f"- Standard Deviation: {np.std(array):.2f}")
+print(f"- Median: {np.median(array):.2f}")
+
+output_file = "random_data_python.bin"
+array.tofile(output_file)
+print(f"Random data written to '{output_file}' successfully!")
